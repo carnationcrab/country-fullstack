8c3c1c9122890a1749895a154272696dea06b6d8
Samantha Martorana <samanthamartorana7@gmail.com>
2020-05-22 15:43:56 -0500

    Add more react elements
    This commit is to add more of the elements needed to start the react
    app. I think a lot of it is probably pretty unnecessary, so I will
    definitely be deleting it later. I am switching directions to mainly
    focusing on taking this from the react forward approach rather than
    the php forward approach I was doing initially.

---
+++ src/app.js
@@ -0,0 +1,16 @@
+//Write your javascript here, or roll your own. It's up to you.
+//Make your ajax call to http://localhost:8765/api/index.php here
+
+import React from 'react';
+import logo from './logo.svg';
+import CountryForm from './country-form/country-form'
+
+function App() {
+  return (
+    <div className="App">
+      <CountryForm />        
+    </div>
+  );
+}
+
+export default App;
