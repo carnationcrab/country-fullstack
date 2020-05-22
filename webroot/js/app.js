9c0532e073e188c902e1f4676393b5d33174d4a9
Samantha Martorana <samanthamartorana7@gmail.com>
2020-05-22 13:16:43 -0500

    Initialize react

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,2 +1,22 @@
 //Write your javascript here, or roll your own. It's up to you.
 //Make your ajax call to http://localhost:8765/api/index.php here
+
+import React from 'react';
+import '../css/main.css';
+
+function App() {
+  return (
+      <div>
+        <title>Countries Search</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="/css/main.css" />
+        <body>
+        <script src="/js/app.js"></script>
+            <h1>Country Search Template</h1>
+            <p>This is here to get you started.</p>
+        </body>
+      </div>
+  );
+}
+
+export default App;
